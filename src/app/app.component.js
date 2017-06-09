6b215bf719cba2b13144b7c8c1a2820ba357b806
charlypy <charles.poquet@isen-lille.fr>
2017-06-09 19:02:50 +0200

    WIP composant weather
    WIP composant weather

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -5,18 +5,69 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+('src/libs/js/Constellation-1.8.1.js');
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
     }
+    AppComponent.prototype.ngOnInit = function () {
+        constellation.initializeClient('http://localhost:8088', '881863589a9ba4cf53a1d5e0d00d649e8cb1dca5', 'readOnly');
+        constellation.onConnectionStateChanged(function (change) {
+            if (change.newState === $.signalR.connectionState.connected) {
+                console.log('connected');
+                constellation.registerStateObjectLink("*", "ForecastIO", "Lille", "*", function (so) {
+                    $scope.$apply(function () {
+                        var maxPrev = 7;
+                        $scope.meteoActuel = createDayliPrev(so.Value.currently);
+                        $scope.listOfprevision = [];
+                        for (var i = 0; i < maxPrev; i++) {
+                            $scope.listOfprevision.push(createDayliPrev(so.Value.hourly.data[i]));
+                        }
+                    });
+                });
+            }
+        });
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "weatherWidget.html",
-    })
+        templateUrl: './weatherWidget.html',
+    }),
+    __metadata("design:paramtypes", [])
 ], AppComponent);
 exports.AppComponent = AppComponent;
+function createDayliPrev(meteoActuSo) {
+    return $scope.meteoActuel = {
+        temperature: meteoActuSo.temperature,
+        humidity: meteoActuSo.humidity,
+        dewPoint: meteoActuSo.dewPoint,
+        windSpeed: meteoActuSo.windSpeed,
+        visibility: meteoActuSo.visibility,
+        couldCover: meteoActuSo.couldCover,
+        precipIntensity: meteoActuSo.precipIntensity,
+        precipProbability: meteoActuSo.precipProbability,
+        pressure: meteoActuSo.pressure,
+        ozone: meteoActuSo.ozone,
+        icon: meteoActuSo.icon,
+        date: moment().format("DD/MM/YYYY"),
+        heure: moment().format("HH:mm:ss"),
+        summary: meteoActuSo.summary
+    };
+}
+var HeroListComponent = (function () {
+    function HeroListComponent(service) {
+        this.service = service;
+    }
+    HeroListComponent.prototype.ngOnInit = function () {
+        this.heroes = this.service.getHeroes();
+    };
+    HeroListComponent.prototype.selectHero = function (hero) { this.selectedHero = hero; };
+    return HeroListComponent;
+}());
+exports.HeroListComponent = HeroListComponent;
 //# sourceMappingURL=app.component.js.map
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,71 @@
 import { Component } from '@angular/core';
-
+import ('src/libs/js/Constellation-1.8.1.js');
 @Component({
   selector: 'my-app',
-  template: `weatherWidget.html`,
+  templateUrl: './weatherWidget.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit {
+
+  constructor() { }
+  
+  ngOnInit() {
+    constellation.initializeClient('http://localhost:8088', '881863589a9ba4cf53a1d5e0d00d649e8cb1dca5', 'readOnly');
+    constellation.onConnectionStateChanged(function (change) {
+      if (change.newState === $.signalR.connectionState.connected) {
+        console.log('connected');
+
+        constellation.registerStateObjectLink("*", "ForecastIO", "Lille", "*", function (so) {
+          $scope.$apply(function () {
+
+            var maxPrev = 7;
+            $scope.meteoActuel = createDayliPrev(so.Value.currently);
+
+            $scope.listOfprevision = [];
+
+            for (var i = 0; i < maxPrev; i++) {
+              $scope.listOfprevision.push(createDayliPrev(so.Value.hourly.data[i]));
+            }
+          });
+        });
+
+      }
+    });
+  }
+
+  function createDayliPrev( meteoActuSo) {
+  return $scope.meteoActuel = {
+    temperature: meteoActuSo.temperature,
+    humidity: meteoActuSo.humidity,
+    dewPoint: meteoActuSo.dewPoint,
+    windSpeed: meteoActuSo.windSpeed,
+    visibility: meteoActuSo.visibility,
+    couldCover: meteoActuSo.couldCover,
+    precipIntensity: meteoActuSo.precipIntensity,
+    precipProbability: meteoActuSo.precipProbability,
+    pressure: meteoActuSo.pressure,
+    ozone: meteoActuSo.ozone,
+    icon: meteoActuSo.icon,
+    date: moment().format("DD/MM/YYYY"),
+    heure: moment().format("HH:mm:ss"),
+    summary: meteoActuSo.summary
+  }
+}
+}
+
+
+
+
+
+
+export class HeroListComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private service: HeroService) { }
+
+  ngOnInit() {
+    this.heroes = this.service.getHeroes();
+  }
+
+  selectHero(hero: Hero) { this.selectedHero = hero; }
+}
